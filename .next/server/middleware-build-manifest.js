012c92588fdd3389bc1c4a3232f167380b85c5a8
LiPoWeRaD <Limon98@list.ru>
2025-04-10 21:56:59 +0300

    fix 3

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-d943ed33de076c47.js","static/chunks/4bd1b696-dc53661a9e7a1ea0.js","static/chunks/517-d3fa638a845a75d1.js","static/chunks/main-app-7fbe4c6be42335ed.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-d943ed33de076c47.js","static/chunks/framework-6b27c2b7aa38af2d.js","static/chunks/main-59f74551634a0a04.js","static/chunks/pages/_app-430fec730128923e.js"],"/_error":["static/chunks/webpack-d943ed33de076c47.js","static/chunks/framework-6b27c2b7aa38af2d.js","static/chunks/main-59f74551634a0a04.js","static/chunks/pages/_error-2d7241423c4a35ba.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/LiPoWeRaDPortfolio",
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   output: "export", 
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
