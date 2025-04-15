11ae25bd2b98f1ebdba4ea0f87d5162a086351c7
LiPoWeRaD <Limon98@list.ru>
2025-04-16 01:35:25 +0300

    add page project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,9 +6,9 @@ const Projects = () => {
     return (
         <main className="grid grid-cols-1 my-auto">
             <ul className="flex flex-wrap gap-[60px] justify-center ">
-                <li><CardProject name="Project 1" nameCommit="ProjectCinema" description="Duis aute irure dolor in velit esse cillum dolore." img={`${process.env.NODE_ENV === 'production' ? '/portfolio' : ''}/images/SiteFilm1.png`} link="https://lipoweradcinema.netlify.app" /></li>
-                <li><CardProject name="Project 2" nameCommit="ProjectBank" description="Duis aute irure dolor in velit esse cillum dolore." img={`${process.env.NODE_ENV === 'production' ? '/portfolio' : ''}/images/SiteFilm2.png`} link="https://lipowerad.github.io/bank/" /></li>
-                <li><CardProject name="Project 3" nameCommit="ProjectBlanchard" description="Duis aute irure dolor in velit esse cillum dolore." img={`${process.env.NODE_ENV === 'production' ? '/portfolio' : ''}/images/SiteFilm3.jpg`} link="https://lipowerad.github.io/Blanchard/" /></li>
+                <li><CardProject name="Project 1" nameCommit="ProjectCinema" description="Firts project on react" img={`${process.env.NODE_ENV === 'production' ? '/portfolio' : ''}/images/SiteFilm1.png`} link="https://lipoweradcinema.netlify.app" /></li>
+                <li><CardProject name="Project 2" nameCommit="ProjectBank" description="Two project on next" img={`${process.env.NODE_ENV === 'production' ? '/portfolio' : ''}/images/SiteFilm2.png`} link="https://lipowerad.github.io/bank/" /></li>
+                <li><CardProject name="Project 3" nameCommit="DigitalAgencyCompany" description="Three project on next" img={`${process.env.NODE_ENV === 'production' ? '/portfolio' : ''}/images/SiteFilm3.jpg`} link="https://lipowerad.github.io/Digital-Agency-Company/" /></li>
             </ul>
         </main>
     )
